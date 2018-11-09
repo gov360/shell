ebfa1f5e77349044261e4f00d33de0fa68a79e91
gov360 <31462703+gov360@users.noreply.github.com>
2018-11-09 21:06:22 +0800

    Update if-root.sh

--- if-root.sh
+++ if-root.sh
@@ -2,7 +2,7 @@
 # Author: xiaoxiao
 # Created Time: 2018/10/19 20:45
 # Script Description: if root
-if [$USER != 'root']
+if [ $USER != 'root' ]
    then
        echo "ERROR: need to be root so that"
        exit 1
