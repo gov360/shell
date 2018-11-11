ad900270016f8dddb7968f3e9753d4145ca52d99
gov360 <31462703+gov360@users.noreply.github.com>
2018-11-11 16:10:24 +0800

    Create if-break.sh

---
+++ if-break.sh
@@ -0,0 +1,11 @@
+#!/bin/env bash 
+# Author: xiaoxiao
+# Created Time: 2018/11/12 14:45
+# Script Description: 
+for i in $(seq 1 9)
+  do
+    echo "$i"
+    if   [[ $i -eq  5 ]]
+      then  break
+    fi    
+done
