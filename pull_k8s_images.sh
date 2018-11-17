7bdcecaa8f303f5c84d9ae67a08f3c64ca56de99
gov360 <31462703+gov360@users.noreply.github.com>
2018-11-17 14:29:47 +0800

    Update pull_k8s_images.sh

--- pull_k8s_images.sh
+++ pull_k8s_images.sh
@@ -5,8 +5,8 @@
 images=(kube-proxy:v1.12.1 kube-scheduler:v1.12.1 kube-controller-manager:v1.12.1
 kube-apiserver:v1.12.1 kubernetes-dashboard-amd64:v1.10.0 
 heapster-amd64:v1.5.4 heapster-grafana-amd64:v5.0.4 heapster-influxdb-amd64:v1.5.2 etcd:3.2.24 coredns:1.2.2 pause:3.1 )
-for imageName in ${images[@]} ; do
-docker pull anjia0532/google-containers.$imageName
-docker tag anjia0532/google-containers.$imageName k8s.gcr.io/$imageName
-docker rmi anjia0532/google-containers.$imageName
+for imageName in "${images[@]}"; do
+docker pull anjia0532/google-containers."$imageName"
+docker tag anjia0532/google-containers."$imageName" k8s.gcr.io/"$imageName"
+docker rmi anjia0532/google-containers."$imageName"
 done
