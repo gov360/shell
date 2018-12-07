548e10a02a7f1b6405cc7cfb562540913c8f580e
gov360 <31462703+gov360@users.noreply.github.com>
2018-12-07 11:50:57 +0800

    Update pull_k8s_images.sh

--- pull_k8s_images.sh
+++ pull_k8s_images.sh
@@ -2,6 +2,33 @@
 # Author: xiaoxiao
 # Created Time: 2018/11/20 14:35
 # Script Description: pull k8s images 
+systemctl stop firewalld
+systemctl disable firewalld
+sed -i 's#SELINUX=enforcing#SELINUX=disabled#g' /etc/selinux/config
+setenforce 0
+swapoff -a
+sed -i 's/.*swap.*/#&/' /etc/fstab
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64/
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+wget https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo -P /etc/yum.repos.d/
+yum list docker-ce.x86_64  --showduplicates |sort -r
+yum install docker-ce-18.06.1.ce -y
+systemctl daemon-reload
+systemctl enable docker
+systemctl start docker
+
 for V in 'kubeadm config images list |grep ^k8s|awk -F : '{ print $NF }'|sed "s/v//"'; do
   if [ $imageName == ^ ]; then
 kube_version=$V
