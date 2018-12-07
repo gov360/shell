c21fe4fdb51f58115f374642c661ac94154e9e4a
gov360 <31462703+gov360@users.noreply.github.com>
2018-12-07 11:20:55 +0800

    Update pull_k8s_images.sh

--- pull_k8s_images.sh
+++ pull_k8s_images.sh
@@ -2,9 +2,13 @@
 # Author: xiaoxiao
 # Created Time: 2018/11/20 14:35
 # Script Description: pull k8s images 
-images=(kube-proxy:v1.12.2 kube-scheduler:v1.12.2 kube-controller-manager:v1.12.2
-kube-apiserver:v1.12.2 kubernetes-dashboard-amd64:v1.10.0 
-heapster-amd64:v1.5.4 heapster-grafana-amd64:v5.0.4 heapster-influxdb-amd64:v1.5.2 etcd:3.2.24 coredns:1.2.2 pause:3.1 )
+for V in 'kubeadm config images list |grep ^k8s|awk -F : '{ print $NF }'|sed "s/v//"'; do
+  if [ $imageName == ^ ]; then
+kube_version=$V
+done
+images=(kube-proxy kube-scheduler kube-controller-manager
+kube-apiserver kubernetes-dashboard-amd64:v1.10.0 
+heapster-amd64:v1.5.4 heapster-grafana-amd64:v5.0.4 heapster-influxdb-amd64:v1.5.2 etcd coredns  pause )
 for imageName in "${images[@]}"; do
 docker pull anjia0532/google-containers."$imageName"
 docker tag anjia0532/google-containers."$imageName" k8s.gcr.io/"$imageName"
