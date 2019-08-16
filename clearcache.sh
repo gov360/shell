53c997a8c93b54736c08016dcc882808df9a4b48
gov360 <969418968@qq.com>
2019-08-17 01:56:59 +0800

    优化系统缓存清理脚本

--- clearcache.sh
+++ clearcache.sh
@@ -3,8 +3,9 @@
 #从内存中取出数据放到硬盘中
 sync
 #表示清除pagecache和slab分配器中的缓存对象。
+echo 1 > /proc/sys/vm/drop_caches
+echo 2 > /proc/sys/vm/drop_caches
 echo 3 > /proc/sys/vm/drop_caches
-echo 0 > /proc/sys/vm/drop_caches
 
 # socket的最大连接数的修改
 # echo 50000 > /proc/sys/net/core/somaxconn
@@ -12,4 +13,3 @@ echo 0 > /proc/sys/vm/drop_caches
 # echo  1 > /proc/sys/net/ipv4/tcp_tw_recycle
 # 允许空的tcp回收利用
 # echo 1 > /proc/sys/net/ipv4/tcp_tw_reuse
-
